057595952da4a4b80d418c585aba2e5ed332dbe0
John Winski <JWINSKI@nait.ca>
2024-09-18 14:07:31 -0600

    Add project files.

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerInfo : MonoBehaviour
+{
+    [SerializeField] int myIntelligence;
+    [SerializeField] int myStrength;
+    [SerializeField] int myWisdom;
+    [SerializeField] int myDexterity;
+
+    [SerializeField] Slider myDexSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerStats.dexterity = 50;
+        PlayerStats.strength = 10;
+        PlayerStats.wisdom = 2;
+        PlayerStats.intelligence = 14;
+
+        myDexterity = PlayerStats.dexterity;
+        myIntelligence = PlayerStats.intelligence;
+        myStrength = PlayerStats.strength;
+        myWisdom = PlayerStats.wisdom;
+
+        myDexSlider.value = myDexterity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DexSliderChange(float value)
+    {
+        myDexterity = (int)value;
+        PlayerStats.dexterity = (int)value;
+    }
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("InfoScreen");
+    }
+}
+
+public static class PlayerStats
+{
+    public static int strength;
+    public static int dexterity;
+    public static int wisdom;
+    public static int intelligence;
+}
